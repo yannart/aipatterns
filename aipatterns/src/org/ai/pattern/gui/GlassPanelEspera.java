8497ef70b031e253cf22b16fd3897396f60a772e
yannart <yannart@dd5ce59b-2e31-0410-9e42-31374be44962>
2007-05-26 02:10:07 +0000

    Se coloco la clase de la pantaalla de espera en un archivo fuente separado para mejorar la claridad del codigo.

---
+++ aipatterns/src/org/ai/pattern/gui/GlassPanelEspera.java
@@ -0,0 +1,124 @@
+/*
+ * GlassPane.java
+ * 
+ * Created on 25 mai 2007, 21:07:04
+ * 
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.ai.pattern.gui;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.MediaTracker;
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JPanel;
+import javax.swing.RootPaneContainer;
+
+/**
+ *
+ * @author yannart
+ */
+public class GlassPanelEspera extends JPanel implements MouseListener, Runnable{
+    private RootPaneContainer rootPane = null;
+    private Component prevGlassPane = null;
+    private boolean drawing = false;
+    private Image[] imagenes;
+    private int numimagen;
+    private Thread thread;
+    private int imageX, imageY, w, h;
+    private MediaTracker tracker;
+    
+    public GlassPanelEspera() {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        imagenes = new Image[8];
+        
+        tracker = new MediaTracker(this);
+        
+        for(int i = 0; i < 8; i++){
+            imagenes[i] = toolkit.getImage(getClass().getResource("/imagenes/indicador" + (i + 1) + ".png"));
+            tracker.addImage(imagenes[i], 0);
+        }
+        
+        while(tracker.statusID(0, true) != MediaTracker.COMPLETE){
+            //se espera0
+        }
+        thread = new Thread(this);
+    }
+    
+    
+    public void setGlassPane(RootPaneContainer rootPane) {
+        this.rootPane = rootPane;
+        prevGlassPane = rootPane.getGlassPane();
+        this.rootPane.setGlassPane(this);
+        setOpaque(false);
+        w = getWidth();
+        h = getHeight();
+        imageY = (h - imagenes[numimagen].getHeight(null))/2;
+        imageX = (w - imagenes[numimagen].getWidth(null))/2;
+    }
+    
+    public void removeGlassPane(){
+        rootPane.setGlassPane(prevGlassPane);
+        setDrawing(false);
+    }
+    
+    public void setDrawing(boolean draw){
+        drawing = draw;
+        setVisible(true);
+        if(draw){
+            addMouseListener(this);
+            thread = new Thread(this);
+            thread.start();
+        }else{
+            removeMouseListener(this);
+        }
+        repaint();
+    }
+    
+    public void paint(Graphics g) {
+        if (drawing) {
+            g.setFont(new Font("Default",Font.BOLD,16));
+            FontMetrics fontMetrics = g.getFontMetrics();
+            int textWidth = fontMetrics.stringWidth("POR FAVOR ESPERE...");
+            int textHeight = fontMetrics.getHeight();
+            
+            g.setColor(new Color(255,255,255,200));
+            g.fillRect(0, 0, w, h);
+            g.drawImage(imagenes[numimagen], imageX, imageY, null);
+            g.setColor(new Color(100,100,100,200));
+            g.drawString("POR FAVOR ESPERE...",w / 2 - textWidth / 2 ,imageY - textHeight);
+        }
+    }
+    
+    public void mouseExited(MouseEvent evt){ }
+    public void mouseEntered(MouseEvent evt){ }
+    public void mousePressed(MouseEvent evt){ }
+    public void mouseReleased(MouseEvent evt){ }
+    public void mouseClicked(MouseEvent evt){ }
+    
+    public void run() {
+        while(drawing){
+            try {
+                thread.sleep(50);
+            } catch (InterruptedException ex) {
+                Logger.getLogger("global").log(Level.SEVERE, null, ex);
+            }
+            
+            numimagen++;
+            if (numimagen >= imagenes.length) {
+                numimagen = 0;
+            }
+            repaint();
+        }
+    }
+}
--- aipatterns/src/org/ai/pattern/gui/MainFrame.java
+++ aipatterns/src/org/ai/pattern/gui/MainFrame.java
@@ -6,25 +6,11 @@
 
 package org.ai.pattern.gui;
 
-import java.awt.Color;
-import java.awt.Component;
 import java.awt.EventQueue;
-import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.MediaTracker;
-import java.awt.Toolkit;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JFileChooser;
 import javax.swing.JInternalFrame;
-import javax.swing.JPanel;
-import javax.swing.RootPaneContainer;
 import org.ai.pattern.Filtrable;
 import org.ai.pattern.Filtro;
 import org.ai.pattern.gui.ImageFrame;
@@ -36,14 +22,14 @@ import org.ai.pattern.gui.ImageFrame;
 public class MainFrame extends javax.swing.JFrame implements Filtrable{
     
     JFileChooser filechooser = new JpgFileChooser();
-    GlassPane glass;
+    GlassPanelEspera glass;
     Filtro filtro;
     FiltroDialog filtrodlg;
     
     /** Creates new form MainFrame */
     public MainFrame() {
         initComponents();
-        glass = new GlassPane();
+        glass = new GlassPanelEspera();
         glass.setGlassPane(this);
     }
     
@@ -63,6 +49,7 @@ public class MainFrame extends javax.swing.JFrame implements Filtrable{
         jMenuItemSalir = new javax.swing.JMenuItem();
         jMenuImagen = new javax.swing.JMenu();
         jMenuItemFiltrar = new javax.swing.JMenuItem();
+        jMenuItemUmbralizar = new javax.swing.JMenuItem();
         jMenuAyuda = new javax.swing.JMenu();
         jMenuItemAbout = new javax.swing.JMenuItem();
 
@@ -104,6 +91,14 @@ public class MainFrame extends javax.swing.JFrame implements Filtrable{
         });
         jMenuImagen.add(jMenuItemFiltrar);
 
+        jMenuItemUmbralizar.setText("Umbralizar");
+        jMenuItemUmbralizar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jMenuItemUmbralizarMousePressed(evt);
+            }
+        });
+        jMenuImagen.add(jMenuItemUmbralizar);
+
         jMenuBar1.add(jMenuImagen);
 
         jMenuAyuda.setText("Ayuda");
@@ -134,6 +129,10 @@ public class MainFrame extends javax.swing.JFrame implements Filtrable{
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+private void jMenuItemUmbralizarMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenuItemUmbralizarMousePressed
+    // TODO add your handling code here:
+}//GEN-LAST:event_jMenuItemUmbralizarMousePressed
     
     
     public void filtrar(float[] matriz){
@@ -221,102 +220,8 @@ private void jMenuItemSalirMousePressed(java.awt.event.MouseEvent evt) {//GEN-FI
     private javax.swing.JMenuItem jMenuItemAbrir;
     private javax.swing.JMenuItem jMenuItemFiltrar;
     private javax.swing.JMenuItem jMenuItemSalir;
+    private javax.swing.JMenuItem jMenuItemUmbralizar;
     private javax.swing.JSeparator jSeparator1;
     // End of variables declaration//GEN-END:variables
     
-}
-
-class GlassPane extends JPanel implements MouseListener, Runnable{
-    private RootPaneContainer rootPane = null;
-    private Component prevGlassPane = null;
-    private boolean drawing = false;
-    private Image[] imagenes;
-    private int numimagen;
-    private Thread thread;
-    private int imageX, imageY, w, h;
-    private MediaTracker tracker;
-    
-    public GlassPane() {
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        imagenes = new Image[8];
-        
-        tracker = new MediaTracker(this);
-        
-        for(int i = 0; i < 8; i++){
-            imagenes[i] = toolkit.getImage(getClass().getResource("/imagenes/indicador" + (i + 1) + ".png"));
-            tracker.addImage(imagenes[i], 0);
-        }
-        
-        while(tracker.statusID(0, true) != MediaTracker.COMPLETE){
-            //se espera0
-        }
-        thread = new Thread(this);
-    }
-    
-    
-    public void setGlassPane(RootPaneContainer rootPane) {
-        this.rootPane = rootPane;
-        prevGlassPane = rootPane.getGlassPane();
-        this.rootPane.setGlassPane(this);
-        setOpaque(false);
-        w = getWidth();
-        h = getHeight();
-        imageY = (h - imagenes[numimagen].getHeight(null))/2;
-        imageX = (w - imagenes[numimagen].getWidth(null))/2;
-    }
-    
-    public void removeGlassPane(){
-        rootPane.setGlassPane(prevGlassPane);
-        setDrawing(false);
-    }
-    
-    public void setDrawing(boolean draw){
-        drawing = draw;
-        setVisible(true);
-        if(draw){
-            addMouseListener(this);
-            thread = new Thread(this);
-            thread.start();
-        }else{
-            removeMouseListener(this);
-        }
-        repaint();
-    }
-    
-    public void paint(Graphics g) {
-        if (drawing) {
-            g.setFont(new Font("Default",Font.BOLD,16));
-            FontMetrics fontMetrics = g.getFontMetrics();
-            int textWidth = fontMetrics.stringWidth("POR FAVOR ESPERE...");
-            int textHeight = fontMetrics.getHeight();
-            
-            g.setColor(new Color(255,255,255,200));
-            g.fillRect(0, 0, w, h);
-            g.drawImage(imagenes[numimagen], imageX, imageY, null);
-            g.setColor(new Color(100,100,100,200));
-            g.drawString("POR FAVOR ESPERE...",w / 2 - textWidth / 2 ,imageY - textHeight);
-        }
-    }
-    
-    public void mouseExited(MouseEvent evt){ }
-    public void mouseEntered(MouseEvent evt){ }
-    public void mousePressed(MouseEvent evt){ }
-    public void mouseReleased(MouseEvent evt){ }
-    public void mouseClicked(MouseEvent evt){ }
-    
-    public void run() {
-        while(drawing){
-            try {
-                thread.sleep(50);
-            } catch (InterruptedException ex) {
-                Logger.getLogger("global").log(Level.SEVERE, null, ex);
-            }
-            
-            numimagen++;
-            if (numimagen >= imagenes.length) {
-                numimagen = 0;
-            }
-            repaint();
-        }
-    }
 }
