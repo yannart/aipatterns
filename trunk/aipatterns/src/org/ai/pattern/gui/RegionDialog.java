85a8d936cd71a7e005448f69e89534184a0e1416
yannart <yannart@dd5ce59b-2e31-0410-9e42-31374be44962>
2007-06-17 02:34:37 +0000

    Se quitaron instrucciones de debugueo. Se cambio el numero de pasada inicial en el dialog de regionalizacion.

--- trunk/aipatterns/src/org/ai/pattern/gui/RegionDialog.java
+++ trunk/aipatterns/src/org/ai/pattern/gui/RegionDialog.java
@@ -44,7 +44,7 @@ public class RegionDialog extends javax.swing.JDialog {
 
         jPanelRegion.setBorder(javax.swing.BorderFactory.createTitledBorder("Numero de pasadas"));
 
-        jSpinnerPasadas.setModel(new javax.swing.SpinnerNumberModel(2, 2, 100, 1));
+        jSpinnerPasadas.setModel(new javax.swing.SpinnerNumberModel(10, 2, 100, 1));
 
         javax.swing.GroupLayout jPanelRegionLayout = new javax.swing.GroupLayout(jPanelRegion);
         jPanelRegion.setLayout(jPanelRegionLayout);
@@ -52,7 +52,7 @@ public class RegionDialog extends javax.swing.JDialog {
             jPanelRegionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanelRegionLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jSpinnerPasadas, javax.swing.GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
+                .addComponent(jSpinnerPasadas, javax.swing.GroupLayout.DEFAULT_SIZE, 176, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanelRegionLayout.setVerticalGroup(
@@ -60,7 +60,7 @@ public class RegionDialog extends javax.swing.JDialog {
             .addGroup(jPanelRegionLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jSpinnerPasadas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(16, Short.MAX_VALUE))
+                .addContainerGap(17, Short.MAX_VALUE))
         );
 
         jButtonAceptar.setText("Aceptar");
@@ -87,7 +87,7 @@ public class RegionDialog extends javax.swing.JDialog {
                     .addComponent(jPanelRegion, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(jButtonCancelar)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 10, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
                         .addComponent(jButtonAceptar)))
                 .addContainerGap())
         );
