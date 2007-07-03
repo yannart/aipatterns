584e42fcf0365e6935379daef75831b7c70433b6
yannart <yannart@dd5ce59b-2e31-0410-9e42-31374be44962>
2007-07-03 22:40:30 +0000

    Se hicieron algunos cambios. Se utiliza la version Milestone 10 de NetBeans.

--- aipatterns/src/org/ai/pattern/BaseConocimientoNumeros.java
+++ aipatterns/src/org/ai/pattern/BaseConocimientoNumeros.java
@@ -14,51 +14,88 @@ package org.ai.pattern;
  * @author yannart
  */
 public enum BaseConocimientoNumeros {
-    
+
     NUM1(1, 0,  52f, 0.578f, 0.27f, 0.009f),
     NUM2(2, 0, 106f, 0.59f, 0.149f, 0.018f),
     NUM4(4, 1, 89f, 0.352f, 0.0254f, 0.016f),
     NUM8(8, 2, 136f, 0.376f, 0.0415f, 6E-4f),
     NUM9(9, 1, 95.5f, 0.32f, 0.01f, 0.0025f);
-    
+
     int valor;
     int hoyos;
     float densidad;
-    float momento1;
-    float momento2;
-    float momento3;
-    
-    BaseConocimientoNumeros(int valor, int hoyos, float densidad, float momento1, float momento2, float momento3){
+    float[] momentos;
+
+    BaseConocimientoNumeros(int valor, int hoyos, float densidad, float momento1, float momento2, float momento3) {
         this.valor = valor;
         this.hoyos = hoyos;
         this.densidad = densidad;
-        this.momento1 = momento1;
-        this.momento2 = momento2;
-        this.momento3 = momento3;
+        momentos = new float[3];
+        momentos[0] = momento1;
+        momentos[1] = momento2;
+        momentos[2] = momento3;
     }
-    
-    public static BaseConocimientoNumeros encuentraNumero(int hoyos, float densidad, float momento1, float momento2, float momento3){
-        if(hoyos == 2 || densidad > 120){
+
+    public static BaseConocimientoNumeros encuentraNumero(int hoyos, float densidad, float momento1, float momento2, float momento3) {
+        float[] momentos = {momento1, momento2, momento3};
+        BaseConocimientoNumeros solucion = null;
+        BaseConocimientoNumeros solucion2 = null;
+        int mejorPuntaje = 0;
+        double menorDiferencia = Double.MAX_VALUE;
+        
+        if(hoyos == 2){
             return NUM8;
-        }else if(hoyos == 0){
-            if(densidad < (NUM1.densidad + NUM2.densidad) / 2.0){
-                return NUM1;
-            }else{
-                return NUM2;
-            }
-        }else if(hoyos == 1) {
-            if(densidad < (NUM4.densidad + NUM9.densidad) / 2.0){
-                return NUM4;
-            }else{
-                return NUM9;
-            }
-        }else{
-            return null;
         }
+        
+        for (BaseConocimientoNumeros numero : BaseConocimientoNumeros.values()) {
+            if(numero.hoyos < hoyos){
+                continue;
+            }
+            
+            int puntaje = 0;
+            double diferenciaCuadratica = 0;
+            
+            diferenciaCuadratica += Math.sqrt(Math.pow(numero.hoyos - hoyos, 2));
+            
+            diferenciaCuadratica += Math.sqrt(Math.pow(numero.densidad - densidad, 2)) / 10;
+            
+            for (int j = 0; j < 3; j++) {
+                diferenciaCuadratica += Math.sqrt(Math.pow(numero.momentos[j] - momentos[j], 2));
+            }
+            
+            if(diferenciaCuadratica < menorDiferencia){
+                menorDiferencia = diferenciaCuadratica;
+                solucion2 = numero;
+            }
+            
+            /*if (numero.hoyos == hoyos) {
+                puntaje++;
+            }
+
+            for (int j = 0; j < 3; j++) {
+                if (momentos[j] < numero.momentos[j] + numero.momentos[j] * 0.1f
+                        && momentos[j] > numero.momentos[j] - numero.momentos[j] * 0.1f) {
+                    puntaje++;
+                }
+            }
+
+            if (densidad < numero.densidad + numero.densidad * 0.1f
+                    && densidad > numero.densidad - numero.densidad * 0.1f) {
+                puntaje++;
+            }
+            
+            if(puntaje > mejorPuntaje){
+                mejorPuntaje = puntaje;
+                solucion = numero;
+            }
+            */
+        }
+        
+        return solucion2;
     }
-    
+
     @Override
-    public String toString(){
+    public String toString() {
         return "" + valor;
     }
 }
